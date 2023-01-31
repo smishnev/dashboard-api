5f12ac1fb408c14cec5d07dbcfe16821f6f76c86
Sergey Mishnev <s.mishnev@forsit.de>
2023-01-31 16:58:48 +0200

    feat: router added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { userRouter } from './users/users.js'
 
 const port = 8000;
 const app = express();
@@ -6,6 +7,8 @@ const app = express();
 app.get('/hello', (req, res) => {
     res.send('Hi all')
 })
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
