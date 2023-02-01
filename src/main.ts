4091f13870a652d00d780cf1cf62844abbda2a5f
Sergey Mishnev <s.mishnev@forsit.de>
2023-02-01 18:34:57 +0200

    feat: implement interface for user controller

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 
-import { Container } from 'inversify';
+import { Container, ContainerModule, interfaces } from 'inversify';
 import { App } from './app'
 import { ExceptionFilter } from './errors/exception.filter';
 import { IExceptionFilter } from './errors/exception.filter.interface';
@@ -8,13 +8,20 @@ import { LoggerService } from './logger/logger.service';
 import { TYPES } from './types';
 import { UserController } from './users/users.controller';
 
-const appContainer = new Container();
-appContainer.bind<ILogger>(TYPES.ILogger).to(LoggerService)
-appContainer.bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter)
-appContainer.bind<UserController>(TYPES.UserController).to(UserController)
-appContainer.bind<App>(TYPES.Application).to(App)
-const app = appContainer.get<App>(TYPES.Application)
-app.init();    
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+    bind<ILogger>(TYPES.ILogger).to(LoggerService)
+    bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter)
+    bind<UserController>(TYPES.UserController).to(UserController)
+    bind<App>(TYPES.Application).to(App)
+})
 
-export { app, appContainer }
+function bootstrap() {
+    const appContainer = new Container();
+    appContainer.load(appBindings)
+    const app = appContainer.get<App>(TYPES.Application)
+    app.init();
+    return { app, appContainer };
+}
+
+export const { app, appContainer } = bootstrap();
 
