aaa831bfe01aea74e2612be69dd260340621bf28
Sergey Mishnev <s.mishnev@forsit.de>
2023-02-01 11:52:37 +0200

    feat: change structure, add logger

---
+++ src/logger/logger.service.ts
@@ -0,0 +1,26 @@
+import { Logger, ISettingsParam } from 'tslog';
+
+export class LoggerService {
+    public logger: Logger<unknown>;
+
+    constructor() {
+		this.logger = new Logger({
+			displayInstanceName: false,
+			displayLoggerName: false,
+			displayFilePath: 'hidden',
+			displayFunctionName: false,
+		} as ISettingsParam<unknown>);
+	}
+
+	log(...args: unknown[]): void {
+		this.logger.info(...args);
+	}
+
+	error(...args: unknown[]): void {
+		this.logger.error(...args);
+	}
+
+	warn(...args: unknown[]): void {
+		this.logger.warn(...args);
+	}
+}
