9e7b0179f38380f165023f2810d35bc789ccc0e5
Sergey Mishnev <s.mishnev@forsit.de>
2023-02-02 13:33:10 +0200

    feat: add validation on middleware

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserRegisterDto {
+	@IsEmail({}, { message: 'Wrong email' })
 	email: string;
+
+	@IsString({ message: 'Password not set' })
 	password: string;
+
+	@IsString({ message: 'Name not set' })
 	name: string;
 }
