90772342f480b3523a6001eb36f402dc65699781
Sergey Mishnev <s.mishnev@forsit.de>
2023-02-02 17:14:01 +0200

    feat: prisma added

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Container, ContainerModule, interfaces } from 'inversify';
 import { App } from './app';
 import { ConfigService } from './config/config.service';
 import { IConfigService } from './config/config.service.interface';
+import { PrismaService } from './database/prisma.service';
 import { ExceptionFilter } from './errors/exception.filter';
 import { IExceptionFilter } from './errors/exception.filter.interface';
 import { ILogger } from './logger/logger.interface';
@@ -23,7 +24,8 @@ export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<IUserController>(TYPES.UserController).to(UserController).inSingletonScope();
 	bind<IUserService>(TYPES.UserService).to(UserService).inSingletonScope();
 	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
-	bind<App>(TYPES.Application).to(App).inSingletonScope();
+	bind<PrismaService>(TYPES.PrismaService).to(PrismaService);
+	bind<App>(TYPES.Application).to(App);
 });
 
 function bootstrap(): IBootstrapReturn {
